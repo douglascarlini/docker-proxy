5f50c4573b50a90a360b743b593da9c3eab9d2cc
Douglas Carlini <douglascarlini@gmail.com>
2023-07-12 01:13:49 -0300

    save

--- example.sh
+++ example.sh
@@ -10,14 +10,10 @@ git reset --hard && git pull
 # Stop and delete all containers
 docker stop $(docker ps -aq) && docker rm $(docker ps -aq)
 
-# Create proxy network
-docker network rm proxy_network
-docker network create proxy_network
-
 # Create and deploy apps containers
-docker run --name app1 --network proxy_network -v $PWD/www/app1:/var/www/html -p 20801:80 -d nginx
-docker run --name app2 --network proxy_network -v $PWD/www/app2:/var/www/html -p 20802:80 -d nginx
-docker run --name app3 --network proxy_network -v $PWD/www/app3:/var/www/html -p 20803:80 -d nginx
+docker run --name app1 --network host -v $PWD/www/app1:/var/www/html -p 20801:80 -d nginx
+docker run --name app2 --network host -v $PWD/www/app2:/var/www/html -p 20802:80 -d nginx
+docker run --name app3 --network host -v $PWD/www/app3:/var/www/html -p 20803:80 -d nginx
 
 # Add permission
 chmod +x deploy.sh
