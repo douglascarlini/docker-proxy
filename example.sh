f15f24aa597775f274478a9ae9602678ae4a71a5
Douglas Carlini <douglascarlini@gmail.com>
2023-07-12 02:45:20 -0300

    save

--- example.sh
+++ example.sh
@@ -13,9 +13,9 @@ git reset --hard && git pull
 { docker stop $(docker ps -aq) && docker rm $(docker ps -aq); } || { echo ""; }
 
 # Create and deploy apps containers
-docker run --name app1 -v $PWD/www/app1:/usr/share/nginx/html -p 20801:80 -d nginx
-docker run --name app2 -v $PWD/www/app2:/usr/share/nginx/html -p 20802:80 -d nginx
-docker run --name app3 -v $PWD/www/app3:/usr/share/nginx/html -p 20803:80 -d nginx
+docker run --name app1 -v $PWD/www/app1:/usr/share/nginx/html -p 20801:80 -d nginx:latest
+docker run --name app2 -v $PWD/www/app2:/usr/share/nginx/html -p 20802:80 -d nginx:latest
+docker run --name app3 -v $PWD/www/app3:/usr/share/nginx/html -p 20803:80 -d nginx:latest
 
 # Add permission
 bash deploy.sh
