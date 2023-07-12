4a1d1334574652386e117f94fa29ad7598a58d4a
Douglas Carlini <douglascarlini@gmail.com>
2023-07-12 01:01:32 -0300

    save

--- example.sh
+++ example.sh
@@ -14,6 +14,9 @@ docker run --name app1 --network proxy_network -v $PWD/www/app1:/var/www/html -p
 docker run --name app2 --network proxy_network -v $PWD/www/app2:/var/www/html -p 20802:80 -d nginx
 docker run --name app3 --network proxy_network -v $PWD/www/app3:/var/www/html -p 20803:80 -d nginx
 
+# Add permission
+chmod +x deploy.sh
+
 # Add apps to proxy
 ./deploy.sh app1.$domain 20801
 ./deploy.sh app2.$domain 20802
